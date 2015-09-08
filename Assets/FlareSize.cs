37ff5e3c7f3dcffacd6f4e4528fa8285e21516f2
Eddy00707 <EdwardMuzhevskiy@gmail.com>
2015-09-08 22:41:02 +0300

    Daily commit
    wonderful flares!

---
+++ Assets/FlareSize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlareSize : MonoBehaviour 
+{
+
+	public Camera main;
+	
+	
+	int value=300;
+	
+	void Start()
+	{
+
+		GetComponent<Light>().color = GetComponent<MeshRenderer>().material.color;
+		GetComponent<LensFlare>().color = GetComponent<Light>().color;
+	}
+
+	void Update () 
+	{
+		
+		
+		float distance = Vector3.Magnitude(gameObject.transform.position - Camera.main.transform.position);
+		gameObject.GetComponent<LensFlare>().brightness = value/distance;
+		
+		
+	} 
+}
